b0604039a2c2444a4e109f1f08f599234ed795e1
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-17 16:05:38 -0500

    created KaraokeMachine class and added functionality to add a new song

--- Karaoke.java
+++ Karaoke.java
@@ -1,3 +1,4 @@
+import com.teamtreehouse.KaraokeMachine;
 import com.teamtreehouse.model.Song;
 import com.teamtreehouse.model.SongBook;
 
@@ -8,13 +9,8 @@ import com.teamtreehouse.model.SongBook;
 public class Karaoke {
 
   public static void main(String[] args) {
-    Song song = new Song(
-      "Michael Jackson",
-      "Beat It",
-      "https://www.youtube.com/watch?v=SaEC9i9QOvk");
       SongBook songBook = new SongBook();
-      System.out.printf("Adding %s %n", song);
-      songBook.addSong(song);
-      System.out.printf("There are %d songs. %n", songBook.getSongCount());
+      KaraokeMachine machine = new KaraokeMachine(songBook);
+      machine.run();
   }
 }
