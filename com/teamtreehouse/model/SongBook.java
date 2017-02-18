0dcf456f0d97a40e4cff4d2c234fbe92cac411af
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-17 19:38:36 -0500

    added a Choose option to KaraokeMachine

--- com/teamtreehouse/model/SongBook.java
+++ com/teamtreehouse/model/SongBook.java
@@ -2,7 +2,11 @@ package com.teamtreehouse.model;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 
 public class SongBook {
   private List<Song> mSongs;
@@ -18,4 +22,27 @@ public class SongBook {
   public int getSongCount() {
     return mSongs.size();
   }
+
+// FIXME: this should be cached!!
+  private Map<String, List<Song>> byArtist() {
+    Map<String, List<Song>> byArtist = new HashMap<>();
+    for (Song song : mSongs) {
+      List<Song> artistSongs = byArtist.get(song.getArtist());
+      if (artistSongs == null) {
+        artistSongs = new ArrayList<>();
+        byArtist.put(song.getArtist(), artistSongs);
+      }
+      artistSongs.add(song);
+    }
+    return byArtist;
+  }
+
+  public Set<String> getArtists() {
+    return byArtist().keySet();
+  }
+
+  public List<Song> getSongsForArtist(String artistName) {
+    return byArtist().get(artistName);
+  }
+
 }
